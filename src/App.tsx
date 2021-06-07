6119eba4196865b732f546dfb5c71eb5e8716209
Nathan Opresnik <nopresnik@me.com>
2021-06-07 19:29:37 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC<Record<string, never>> = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <a href="#" target="_blank">
+        Click me
+      </a>
+    </>
+  );
+};
+
+export default App;
