66f3182679cc175e22877fcff96706582527a945
Nathan Opresnik <nopresnik@me.com>
2021-06-10 13:26:43 +1000

    Make multi windows work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Main from './Views/Main';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Switch from 'react-bootstrap/esm/Switch';
 
 const App: React.FC<Record<string, never>> = () => {
   return (
-    <>
-      <Main />
-    </>
+    <Router>
+      <Switch className="m-0 p-0">
+        <Route exact path="/test">
+          <h1>Hello from test route</h1>
+        </Route>
+        <Route exact path="/">
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/Components/JobLists/gridevents.ts
+++ src/Components/JobLists/gridevents.ts
@@ -2,7 +2,9 @@ import RowData from '../../Types/IRowData';
 
 // TODO: OPEN JOB WINDOW
 const openJob = (params: RowData): void => {
-  alert('It works: ' + params.data.jobID);
+  // alert('It works: ' + params.data.jobID);
+  window.open('#/test', '_blank', `title=View Job ${params.data.jobID}`);
+  // handleNewWindow();
 };
 
 export default { openJob };
