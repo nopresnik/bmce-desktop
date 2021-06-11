207511c11f5c1d9d0dc6e035920847d93e6284a2
Nathan Opresnik <nopresnik@me.com>
2021-06-11 19:24:47 +1000

    Update state from forms

--- src/Views/Job/JobForm.tsx
+++ src/Views/Job/JobForm.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+// TODO: Add type safety.  Remove use of any in future
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
 import { Col, Form, Row, Table } from 'react-bootstrap';
+import Address from '../../Types/IAddress';
 import Job from '../../Types/IJob';
 import JobStatus from '../../Types/IJobStatus';
 
@@ -7,6 +10,18 @@ interface PropTypes {
   job: Job;
   setJob: React.Dispatch<React.SetStateAction<Job>>;
 }
+
+const handleChange = (e: any, data: any, setData: any) => {
+  const fieldName: string = e.target.name;
+  const value: string = e.target.value;
+  setData({ ...data, [fieldName]: value });
+};
+
+const handleCheckChange = (e: any, data: any, setData: any) => {
+  const fieldName: string = e.target.name;
+  const value: string = e.target.checked;
+  setData({ ...data, [fieldName]: value });
+};
 
 const JobForm: React.FC<PropTypes> = (props) => {
   const { job, setJob } = props;
@@ -32,7 +47,17 @@ const JobForm: React.FC<PropTypes> = (props) => {
 };
 
 const JobLocationForm: React.FC<PropTypes> = (props) => {
-  const { job } = props;
+  const { job, setJob } = props;
+  const [location, setLocation] = useState<Address>();
+
+  useEffect(() => {
+    setLocation(job.location);
+  }, []);
+
+  useEffect(() => {
+    setJob({ ...job, location: { ...location } });
+  }, [location]);
+
   return (
     <>
       <Form.Group>
@@ -42,6 +67,8 @@ const JobLocationForm: React.FC<PropTypes> = (props) => {
           placeholder="Address line 1"
           size="sm"
           value={job.location.line1 || ''}
+          name="line1"
+          onChange={(e) => handleChange(e, location, setLocation)}
         />
       </Form.Group>
       <Form.Group>
@@ -50,6 +77,8 @@ const JobLocationForm: React.FC<PropTypes> = (props) => {
           placeholder="Address line 2"
           size="sm"
           value={job.location.line2 || ''}
+          name="line2"
+          onChange={(e) => handleChange(e, location, setLocation)}
         />
       </Form.Group>
       <Form.Group>
@@ -58,6 +87,8 @@ const JobLocationForm: React.FC<PropTypes> = (props) => {
           placeholder="City"
           size="sm"
           value={job.location.city || ''}
+          name="city"
+          onChange={(e) => handleChange(e, location, setLocation)}
         />
       </Form.Group>
       <Form.Row>
@@ -67,6 +98,8 @@ const JobLocationForm: React.FC<PropTypes> = (props) => {
             placeholder="State"
             size="sm"
             value={job.location.state || ''}
+            name="state"
+            onChange={(e) => handleChange(e, location, setLocation)}
           />
         </Form.Group>
         <Form.Group as={Col} sm={4}>
@@ -75,6 +108,8 @@ const JobLocationForm: React.FC<PropTypes> = (props) => {
             placeholder="Postcode"
             size="sm"
             value={job.location.postcode || ''}
+            name="postcode"
+            onChange={(e) => handleChange(e, location, setLocation)}
           />
         </Form.Group>
       </Form.Row>
@@ -83,7 +118,7 @@ const JobLocationForm: React.FC<PropTypes> = (props) => {
 };
 
 const JobNotesForm: React.FC<PropTypes> = (props) => {
-  const { job } = props;
+  const { job, setJob } = props;
   return (
     <>
       <Form.Group>
@@ -94,6 +129,8 @@ const JobNotesForm: React.FC<PropTypes> = (props) => {
           placeholder="Notes"
           size="sm"
           value={job.description || ''}
+          name="description"
+          onChange={(e) => handleChange(e, job, setJob)}
         />
       </Form.Group>
       <Form.Group>
@@ -104,6 +141,8 @@ const JobNotesForm: React.FC<PropTypes> = (props) => {
           placeholder="Additional notes"
           size="sm"
           value={job.notes || ''}
+          name="notes"
+          onChange={(e) => handleChange(e, job, setJob)}
         />
       </Form.Group>
       <Form.Group>
@@ -113,6 +152,8 @@ const JobNotesForm: React.FC<PropTypes> = (props) => {
           placeholder="Additional notes"
           size="sm"
           value={job.purchaseOrder || ''}
+          name="purchaseOrder"
+          onChange={(e) => handleChange(e, job, setJob)}
         />
       </Form.Group>
     </>
@@ -225,7 +266,7 @@ const JobPrevRefForm: React.FC<PropTypes> = (props) => {
 };
 
 const JobStatusForm: React.FC<PropTypes> = (props) => {
-  const { job } = props;
+  const { job, setJob } = props;
 
   const testStatus = (status: JobStatus) => job.status == status;
 
@@ -239,6 +280,7 @@ const JobStatusForm: React.FC<PropTypes> = (props) => {
           id="active"
           label="Active"
           checked={testStatus(JobStatus.Active)}
+          onChange={() => setJob({ ...job, status: JobStatus.Active })}
         />
         <Form.Check
           custom
@@ -246,6 +288,7 @@ const JobStatusForm: React.FC<PropTypes> = (props) => {
           id="hold"
           label="On Hold"
           checked={testStatus(JobStatus.Hold)}
+          onChange={() => setJob({ ...job, status: JobStatus.Hold })}
         />
         <Form.Check
           custom
@@ -253,6 +296,7 @@ const JobStatusForm: React.FC<PropTypes> = (props) => {
           id="completed"
           label="Completed"
           checked={testStatus(JobStatus.Completed)}
+          onChange={() => setJob({ ...job, status: JobStatus.Completed })}
         />
         <Form.Row className="d-flex align-center">
           <Form.Group as={Col}>
@@ -260,11 +304,21 @@ const JobStatusForm: React.FC<PropTypes> = (props) => {
               type="date"
               size="sm"
               value={new Date(job.dateCompleted).toISOString().substr(0, 10)}
+              name="dateCompleted"
+              onChange={(e) => handleChange(e, job, setJob)}
+              disabled={job.status !== JobStatus.Completed}
             />
           </Form.Group>
           <Form.Group as={Col} className="d-flex">
             <Form.Label className="mr-2">By</Form.Label>
-            <Form.Control type="select" size="sm" value={job.completedBy} />
+            <Form.Control
+              type="select"
+              size="sm"
+              value={job.completedBy}
+              name="completedBy"
+              onChange={(e) => handleChange(e, job, setJob)}
+              disabled={job.status !== JobStatus.Completed}
+            />
           </Form.Group>
         </Form.Row>
       </Form.Group>
@@ -273,7 +327,7 @@ const JobStatusForm: React.FC<PropTypes> = (props) => {
 };
 
 const JobInvoiceForm: React.FC<PropTypes> = (props) => {
-  const { job } = props;
+  const { job, setJob } = props;
 
   return (
     <Form.Group>
@@ -286,6 +340,8 @@ const JobInvoiceForm: React.FC<PropTypes> = (props) => {
             id="invoiced"
             label="Invoiced"
             checked={job.invoiced === true}
+            name="invoiced"
+            onChange={(e) => handleCheckChange(e, job, setJob)}
           />
         </Form.Group>
         <Form.Group as={Col}>
@@ -295,6 +351,8 @@ const JobInvoiceForm: React.FC<PropTypes> = (props) => {
             id="paid"
             label="Invoice Paid"
             checked={job.invoicePaid === true}
+            name="invoicePaid"
+            onChange={(e) => handleCheckChange(e, job, setJob)}
           />
         </Form.Group>
       </Form.Row>
